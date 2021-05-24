feba3f61d7ace127928be2b69041e425bca664d0
abhashkumar <abhashmaddi@hotmail>
2021-05-24 22:36:04 +0530

    making project debuggable

--- COvin.ts
+++ COvin.ts
@@ -1,5 +1,5 @@
 import get from 'got';
-import { sendEmail, mailObj} from './NotifyByMail';
+import { sendEmail, mailObj } from './NotifyByMail';
 import { retrieveUserIdObjects } from './dbUtil';
 import moment from 'moment';
 
@@ -11,22 +11,22 @@ async function callAPI() {
   let response = await get(url);
   var centreList = JSON.parse(response.body);
   var centerWithAvailablility: any[] = [];
-  _.each(centreList.centers, function(center:any) {
+  _.each(centreList.centers, function (center: any) {
     var sessions = center.sessions;
-    _.each(sessions, function(session: any){
-      if(session.min_age_limit >= 18 && session.available_capacity > 0) {
+    _.each(sessions, function (session: any) {
+      if (session.min_age_limit >= 18 && session.available_capacity > 0) {
         console.log('vaccine available');
         console.log(center.pincode);
-        var location = {"address": center.address, "pincode":center.pincode};
+        var location = { "address": center.address, "pincode": center.pincode };
         centerWithAvailablility.push(location);
         return false;
       }
     });
   });
-  if(centerWithAvailablility.length > 0) {
-    var user:any = [];
-    var res:any = await retrieveUserIdObjects();
-    user = res.map((user_:any) => user_.emailId);
+  if (centerWithAvailablility.length > 0) {
+    var user: any = [];
+    var res: any = await retrieveUserIdObjects();
+    user = res.map((user_: any) => user_.emailId);
     mailObj.to = String(user);
     mailObj.subject = 'vaccine available';
     mailObj.html = JSON.stringify(centerWithAvailablility, null, 2);
@@ -34,4 +34,4 @@ async function callAPI() {
   }
   console.log('call completed');
 }
-setInterval(callAPI, 30000 );
+setInterval(callAPI, 30000);
